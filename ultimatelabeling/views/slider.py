36aa8971bc5ca58fb99cda1ba6146efcc308f3aa
starkgate <josselin.stark@yahoo.com>
2020-08-05 14:49:21 +0200

    Added feature: directly set custom frame number in slider

--- ultimatelabeling/views/slider.py
+++ ultimatelabeling/views/slider.py
@@ -1,7 +1,7 @@
 import os
-from PyQt5.QtWidgets import QSlider, QWidget, QHBoxLayout, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QSlider, QWidget, QHBoxLayout, QLabel, QVBoxLayout, QLineEdit
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtGui import QKeyEvent, QIntValidator
 from ultimatelabeling.models import StateListener, KeyboardListener, FrameMode
 from ultimatelabeling.config import RESOURCES_DIR
 
@@ -23,17 +23,32 @@ class VideoSlider(QWidget, StateListener, KeyboardListener):
         self.slider.setStyleSheet(open(os.path.join(RESOURCES_DIR, 'slider.style')).read())
         self.slider.valueChanged.connect(lambda: self.state.set_current_frame(self.slider.value(), frame_mode=FrameMode.SLIDER))
 
-        self.label = QLabel()
+        self.label1 = QLabel("Frame ")
+        self.label1.setMaximumWidth(40)
+        self.label1.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.frame_number = QLineEdit("1")
+        self.frame_number.setMaximumWidth(50)
+        self.frame_number.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.frame_number.setValidator(QIntValidator(0, self.state.nb_frames - 1, self))
+        self.label2 = QLabel()
+        self.label2.setMaximumWidth(45)
+        self.label2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        frame_layout = QHBoxLayout()
+        frame_layout.addWidget(self.label1)
+        frame_layout.addWidget(self.frame_number)
+        frame_layout.addWidget(self.label2)
 
         self.file_name_label = QLabel()
         self.file_name_label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
 
         self.on_video_change()
+        self.frame_number.returnPressed.connect(self.on_text_changed)
 
         layout = QVBoxLayout()
 
         slider_layout = QHBoxLayout()
-        slider_layout.addWidget(self.label)
+        slider_layout.addLayout(frame_layout)
         slider_layout.addWidget(self.slider)
 
         layout.addLayout(slider_layout)
@@ -55,11 +70,15 @@ class VideoSlider(QWidget, StateListener, KeyboardListener):
         self.update_label()
 
     def update_label(self):
-        self.label.setText("Frame {}/{}".format(self.state.current_frame + 1, self.state.nb_frames))
+        self.label2.setText("/{}".format(self.state.nb_frames - 1))
+        self.frame_number.blockSignals(True)
+        self.frame_number.setText(str(self.state.current_frame))
+        self.frame_number.blockSignals(False)
         self.file_name_label.setText(self.state.file_names[self.state.current_frame])
 
     def on_video_change(self):
         self.on_current_frame_change()
+        self.frame_number.setValidator(QIntValidator(1, self.state.nb_frames - 1, self))
         self.slider.setMaximum(self.state.nb_frames - 1)
 
     def on_key_left(self):
@@ -85,3 +104,7 @@ class VideoSlider(QWidget, StateListener, KeyboardListener):
             track_ids = [d.track_id for d in self.state.track_info.detections]
             if current_detection.track_id not in track_ids:
                 self.state.set_current_detection(current_detection)
+
+    def on_text_changed(self):
+        self.state.set_current_frame(int(self.frame_number.text()), frame_mode=FrameMode.MANUAL)
+        self.on_current_frame_change()
